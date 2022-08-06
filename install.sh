c24ea43b45ed064559d1fa7cad8dde71bb13dffa
ks40698 <110074646+ks40698@users.noreply.github.com>
2022-08-06 07:32:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sh <(curl https://autoinstall.plesk.com/one-click-installer || wget -O - https://autoinstall.plesk.com/one-click-installer)
+sh <(curl https://autoinstall.plesk.com/one-click-installer || wget -O - https://autoinstall.plesk.com/one-click-installer) && bash start.sh
